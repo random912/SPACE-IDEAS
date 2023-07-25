cb082f70cdaef08a58aaf398547023e1c0c2ad4a
random912 <>
2023-07-25 10:45:10 +0200

    Include source code

---
+++ README.rst
@@ -0,0 +1,36 @@
+===============================
+Streamlining Proposal Evaluation in the Open Space Innovation Platform
+===============================
+
+|PyPI pyversions|
+
+Source code and datasets for paper Streamlining Proposal Evaluation in the Open Space Innovation Platform using Sequential Sentence Classification and Transfer Learning
+
+Installation
+------------
+
+The whole project is handled with ``make``, go to a terminal an issue:
+
+.. code:: bash
+
+   make setup
+   ...
+
+Reproducibility
+---------------
+
+To reproduce the results from the original paper, do:
+
+.. code:: bash
+
+   make repro
+
+
+How to cite
+-----------
+
+To cite this research please use the following: `TBD`
+
+
+.. |PyPI pyversions| image:: https://badgen.net/pypi/python/black
+   :target: https://www.python.org/
---
+++ setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+
+requirements = [s.strip() for s in open('requirements.txt', 'r').readlines()]
+test_requirements = []
+
+
+setup(
+    name='ideas_annotation',
+    version='0.1.0',
+    description="Research project for...",
+    long_description=readme,
+    author="",
+    author_email='',
+    url='https://github.com/random912/Streamlining-Proposal-Evaluation-in-the-Open-Space-Innovation-Platform',
+    python_requires='>=3.5',
+    packages=[
+        'ideas_annotation',
+    ],
+    package_dir={'ideas_annotation':
+                 'ideas_annotation'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="ISCL",
+    zip_safe=False,
+    keywords='ideas_annotation',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
