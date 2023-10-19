112d6cb1cbcdd475bf4e781fb45f35f5873b8e87
random912 <132650772+random912@users.noreply.github.com>
2023-10-19 13:21:06 +0200

    Update split_data.py

--- scripts/split_data.py
+++ scripts/split_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-data = pd.read_json("data/processed/osip.jsonl",lines=True)
+data = pd.read_json("data/processed/space-ideas.jsonl",lines=True)
 train, test = train_test_split(data, test_size=0.2, random_state=42)
                                
 with open("data/processed/train.jsonl", "w") as f:
