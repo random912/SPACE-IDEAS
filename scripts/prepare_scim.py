58dce1cd061fcdfa1aef3cf815c1f6b567e0fa4c
random912 <132650772+random912@users.noreply.github.com>
2023-10-20 12:20:03 +0200

    Delete scripts/prepare_scim.py

--- scripts/prepare_scim.py
+++
@@ -1,8 +0,0 @@
-from datasets import load_dataset, DatasetDict
-
-dataset = load_dataset("scim/naacl_data_prog")
-dataset = dataset.map(lambda example: {"sentences":example["sentences"][:10],"labels":example["labels"][:10]})
-dataset_split = dataset["train"].train_test_split(0.2)
-test_valid = dataset_split["test"].train_test_split(0.5)
-dataset = DatasetDict({"train":dataset_split["train"],"validation":test_valid["train"],"test":test_valid["test"]})
-dataset.save_to_disk("data/processed/scim")
